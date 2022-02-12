2d107f2e94dfe6e56e31d4538908cce483c971fc
krutoi-muzhik <71013428+krutoi-muzhik@users.noreply.github.com>
2022-02-12 04:50:20 +0300

    Add files via upload

---
+++ myls.c
@@ -0,0 +1,226 @@
+#define _GNU_SOURCE
+#include <dirent.h>     /* Defines DT_* constants */
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <stdint.h>
+#include <linux/types.h>
+#include <time.h>
+#include <pwd.h>
+#include <getopt.h>
+#include <string.h>
+
+
+#define handle_error(cmd) \
+	do { perror (cmd); exit (EXIT_FAILURE); } while (0)
+
+struct linux_dirent {
+	long d_ino;
+	off_t d_off;
+	unsigned short d_reclen;
+	char d_name[];
+};
+
+struct linux_dirent64 {
+	ino64_t d_ino;
+	off64_t d_off;
+	unsigned short d_reclen;
+	unsigned char d_type;
+	char d_name[];
+};
+
+#define BUF_SIZE 1024
+
+size_t IsDir (struct linux_dirent64 *d64);
+char *PathUnite (const char *s1, const char *s2);
+void LS (const char *name, int R, int a, int l, int i);
+void PrintInode (struct linux_dirent64 *d64);
+void PrintLong (struct linux_dirent64 *d64, const char *pathname);
+
+int main (int argc, char* argv[]) {
+	int opt = 0;
+	int R = 0, a = 0, l = 0, i = 0;	
+
+	while ((opt = getopt (argc, argv, "Rali")) != -1) {
+		switch (opt) {
+			case 'R':
+				R = 1;
+				break;
+			case 'a':
+				a = 1;
+				break;
+			case 'l':
+				l = 1;
+				break;
+			case 'i':
+				i = 1;
+				break;
+			default:
+				printf ("Usage of unknown flag \nOnly -R -a -l -i flags are supported\n");
+				exit (EXIT_FAILURE);
+		}
+	}
+	
+	LS (argc > optind ? argv[optind] : ".", R, a, l, i);
+	exit (EXIT_SUCCESS);
+}
+
+void LS (const char *name, int R, int a, int l, int i) {
+	int fd, nread;
+	char buf[BUF_SIZE];
+	struct linux_dirent *d;
+	struct linux_dirent64 *d64;
+	int bpos = 0;
+	char d_type;
+	struct passwd *pws;
+
+	char *pathname = (char *) calloc (1024, sizeof (char));
+	strcpy (pathname, name);
+
+	int str = strcmp (name, ".");
+
+	if ((fd = open (name, O_RDONLY | O_DIRECTORY)) == -1)
+		handle_error ("open");
+	if (R)
+		printf ("%s/:\n", name);
+
+	do {
+		bpos = 0;
+		if ((nread = getdents64 (fd, buf, BUF_SIZE)) == -1)
+			handle_error ("getdents64");
+
+		while (bpos < nread) {
+			d64 = (struct linux_dirent64 *) (buf + bpos);
+			bpos += d64->d_reclen;
+
+			if (!a && (d64->d_name[0] == '.'))
+				continue;
+
+			if (i) {
+				if (l)
+					printf ("%17ld  ", d64->d_ino);
+				else
+					printf ("%ld  ", d64->d_ino);
+			}
+
+			if (str)
+				pathname = PathUnite (name, d64->d_name);
+
+			if (l)
+				PrintLong (d64, pathname);
+			else 
+				printf ("%s  ", d64->d_name);
+		}
+		printf ("\n");
+
+		if (R) {
+			bpos = 0;
+			while (bpos < nread) {
+				d64 = (struct linux_dirent64 *) (buf + bpos);
+				bpos += d64->d_reclen;
+
+				if (d64->d_name[0] == '.') {
+					continue;
+				}
+
+				if (IsDir (d64)) {
+					printf ("\n");
+					pathname = PathUnite (name, d64->d_name);
+					LS (pathname, R, a, l, i);
+				}
+			}
+		}
+	} while (!nread);
+
+	if (close (fd) == -1)
+		handle_error ("close");
+	return;
+}
+
+void PrintLong (struct linux_dirent64 *d64, const char *pathname) {
+	struct stat statbuf;
+	if (stat (pathname, &statbuf) == -1) {
+		printf ("\n\n%s\n\n", pathname);
+		handle_error ("stat");
+	}
+
+	mode_t mode = statbuf.st_mode;
+	for (int i = 1; i < 512; i *= 2) {
+		if (mode & i) printf ("r");
+		else printf ("-");
+		i *= 2;
+		if (mode & i) 
+			printf ("w");
+		else 
+			printf ("-");
+		i *= 2;
+		if (mode & i) printf ("x");
+		else printf ("-");
+	}
+
+	printf ("  ");
+	printf ("%8ld  ", statbuf.st_size);
+	switch (d64->d_type) {
+		case DT_BLK:
+			printf ("block device");
+			break;
+		case DT_CHR:
+			printf ("character device");
+			break;
+		case DT_DIR:
+			printf ("directory");
+			break;
+		case DT_FIFO:
+			printf ("fifo     ");
+			break;
+		case DT_LNK:
+			printf ("softlink ");
+			break;
+		case DT_REG:
+			printf ("  file   ");
+			break;
+		case DT_SOCK:
+			printf ("UNIX domain socket");
+			break;
+		case DT_UNKNOWN:
+			printf ("unknown type");
+			break;
+		default:
+			printf ("????????????");
+	}
+	printf ("  ");
+
+	struct passwd *pws;
+	if ((pws = getpwuid (statbuf.st_gid)) == NULL)
+		handle_error ("getpwuid");
+	printf ("%s (%d)  ", pws->pw_name, pws->pw_uid);
+
+	char *time = ctime (&statbuf.st_mtime);
+	time[strlen(time) - 1] = ' ';
+	printf ("%s ", time);
+	printf ("%s\n", d64->d_name);
+	
+	return;
+}
+
+char *PathUnite (const char *s1, const char *s2) {
+	size_t len1 = strlen (s1);
+	size_t len2 = strlen (s2);
+
+	char *result = calloc (len1 + len2 + 1, sizeof (char));
+	memcpy (result, s1, len1);
+	result[len1] = '/';
+	memcpy (result + len1 + 1, s2, len2);
+	//printf ("\n\n\n%s\n\n\n", result);
+	//result[len1 + len2 + 1] = ' ';
+
+	return result;
+}
+
+size_t IsDir (struct linux_dirent64 *d64) {
+	return (d64->d_type == DT_DIR);
+}
